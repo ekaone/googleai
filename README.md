18e0c53c68d61871b18e8355bfb83555e6add8d5
Eka Prasetia <ekaone3033@gmail.com>
2024-02-21 11:01:51 +0700

    Add AI model responses and image processing functionality

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 Demo [Google AI](https://ai.google.dev/docs)
+
+# Response
+
+## Based on images provided (birds), below are the responses from the model:
+- Prompt: "What's different between these pictures?"
+- Response: "The first picture is of a volcano. The second picture is of a snow-capped mountain range."
+
+## Based on images provided (mountains), below are the responses from the model:
+- Prompt: "What's the animals name and count the numbers, on these pictures?"
+- Response: "The first picture is of three parrots and the second picture is of six birds"
--- app.js
+++ app.js
@@ -5,14 +5,12 @@ const { log } = console;
 
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
-  // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
-  const prompt = "Write a story about a magic backpack.";
+  const prompt = "Write a poem universe under 100 words.";
 
   const result = await model.generateContent(prompt);
   const response = await result.response;
